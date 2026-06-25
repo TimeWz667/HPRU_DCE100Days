46bf78f41ff1395eacfd4299d6f8b26ab9cde5c9
Chu-ChangKu <timewz667@gmail.com>
2026-06-25 11:01:23 +0100

    Add vaccine ava

--- scripts/1_choiceset_design_pilot.R
+++ scripts/1_choiceset_design_pilot.R
@@ -7,9 +7,36 @@ library(jsonlite)
 attributes <- jsonlite::read_json(here::here("def", "attributes.json"))
 
 
-lvls <- sapply(attributes, \(x) {x$levels %>% length})
-code <- lapply(attributes, \(x) {x$levels %>% sapply(\(d) d$value)})
+lvls <- sapply(attributes, \(x) {x$levels %>% length})[1:4]
+code <- lapply(attributes, \(x) {x$levels %>% sapply(\(d) d$value)})[1:4]
 
 
 cs <- Profiles(lvls = lvls, coding = rep("C", length(lvls)), c.lvls = code)
+
+cs
+
+
+priors <- local({
+  n_pars <- ncol(cs)
+  mu <- rep(0, n_pars)
+  v <- diag(length(mu))
+  pd <- MASS::mvrnorm(n = 50, mu = mu, Sigma = v) # 10 draws.
+  #p.d <- list(matrix(pd[,1], ncol = 1), pd[,2:n_pars])
+  #p.d
+  pd
+})
+
+
+mf_cs <- Modfed(
+  cs,
+  n.set = 48, 
+  n.alts = 2,
+  n.blocks = 6,
+  alt.cte = c(0, 0),
+  par.draws = priors,
+  parallel = T
+)
+
+
+mf_cs
 
